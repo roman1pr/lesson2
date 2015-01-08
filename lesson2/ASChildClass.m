134f11c11d7a4c4beca57056f584ac8a15e657d9
roman1pr <iroman.pri@gmail.com>
2015-01-09 00:47:45 +0300

    add method returned PRIVET

--- lesson2/ASChildClass.m
+++ lesson2/ASChildClass.m
@@ -14,6 +14,11 @@
 - (NSString*) saySomething{
     NSString* string = @"Something from child class";
     return string;
+    
+}
+
+- (NSString*) sayForSomething{
+        return @"privet!";
 }
 
 @end
